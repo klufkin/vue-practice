eaee2d815d1293d50587fb8944523fe6734f09a1
Kevin Lufkin <klufkin11@gmail.com>
2016-11-02 22:31:36 -0400

    implement list update/PUT

--- ajax.js
+++ ajax.js
@@ -12,7 +12,7 @@ function getTodos() {
 }
 
 function postTodo(todoText) {
-    fetch(urlBranch + fAuth, {
+    return fetch(urlBranch + fAuth, {
         method: 'POST',
         body: JSON.stringify({
             title: todoText,
@@ -20,14 +20,27 @@ function postTodo(todoText) {
         })
     }).then(function(data) {
         console.log('Request success: ', data);
-    }).catch(function(error) {
-        console.log('Request failure: ', error);
-    });
+        return data.json();
+    })
 }
 
 function deleteTodo(todoId) {
     fetch(urlTarget + todoId + '.json' + fAuth, {
         method: 'DELETE'
+    }).then(function(data) {
+        console.log('Request success: ', data);
+    }).catch(function(error) {
+        console.log('Request failure: ', error);
+    });
+}
+
+function updateTodo(todoText, todoId) {
+    fetch(urlTarget + todoId + '.json' + fAuth, {
+        method: 'PUT',
+        body: JSON.stringify({ // note if you do not supply property it will be removed
+            title: todoText,
+            visible: true
+        })
     }).then(function(data) {
         console.log('Request success: ', data);
     }).catch(function(error) {
--- toVueList.js
+++ toVueList.js
@@ -1,7 +1,7 @@
 Vue.component('todo-item', {
     props: ['todo'],
     template: `<li>
-            <span  v-if="todo.visible"> {{ todo.title }} </span>
+            <input  v-if="todo.visible" v-on:blur="$emit(\`update\`)" v-bind:value="todo.title" v-model="todo.title">
             <button v-on:click="$emit(\'remove\')">remove</button>
         </li>
         `
@@ -23,16 +23,24 @@ let todos = new Vue({
     },
     methods: {
         add: function() {
-            this.todos.push({
-                title: this.newTodoText,
-                visible: true
-            });
-            postTodo(this.newTodoText); // post to database
-            this.newTodoText = '';
+            // require respone from post get the id of the newly created post
+            // supplying the id allows us to update these new posts successfully
+            postTodo(this.newTodoText).then(function(response) {
+                todos.todos.push({
+                    title: todos.newTodoText,
+                    visible: true,
+                    id: response.name
+                });
+                todos.newTodoText = '';
+            }); // post to database
         },
         remove: function(index) {
             deleteTodo(this.todos[index].id); // runs delete AJAX call
             this.todos.splice(index, 1); // removes from list
+        },
+        update: function(index) {
+            // this.todos[index].title =
+            updateTodo(this.todos[index].title, this.todos[index].id);
         }
     }
 });
